6710ea26631cc4a45aad47e43bf51cdc765990f4
maurice <maurice@nwt-debian-03>
2025-02-28 23:55:32 +0100

    Removed the old files from configuration

--- ddproxies/compile.sh
+++ ddproxies/compile.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-g++ -std=c++17 -o guac-proxy-ingress guac-proxy-ingress.cpp -I.
-g++ -std=c++17 -o guac-proxy-egress guac-proxy-egress.cpp -I.
 g++ -std=c++17 -o gmserver gmserver.cpp -I.
 g++ -std=c++17 -o gmproxyin gmproxyin.cpp -I.
 g++ -std=c++17 -o gmproxyout gmproxyout.cpp -I.
