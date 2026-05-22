70693c688e8b4536a5ad192679b2c9590b8fe731
simon <195196563+sdcock@users.noreply.github.com>
2026-05-22 10:43:48 +0200

    added tcpclient

---
+++ 3dd/src/network/tcpclient.cpp
@@ -0,0 +1,97 @@
+#include "../../include/network/tcpclient.h"
+#include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+
+TCPClient::~TCPClient() {
+    if (recv_sock_fd >= 0) {
+        ::shutdown(recv_sock_fd, SHUT_RDWR);
+        ::close(recv_sock_fd);
+    }
+}
+
+int TCPClient::Initialize() {
+    // int one = 1;
+    // ::setsockopt(recv_sock_fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+
+    struct addrinfo hints, *res, *p;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; //AF_UNSPEC IPv6 not yet
+    hints.ai_socktype = SOCK_STREAM;  // TCP
+
+    std::string port_str = std::to_string(server_port);
+
+    int status = getaddrinfo(server_ip.c_str(), port_str.c_str(), &hints, &res);
+    if (status != 0) {
+        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        return -1;
+    }
+
+    int ret = -1;
+    for (p = res; p != nullptr; p = p->ai_next) {
+        recv_sock_fd = ::socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (recv_sock_fd == -1)
+            continue;
+
+        if (::connect(recv_sock_fd, p->ai_addr, p->ai_addrlen) == 0) {
+            ret = 0; // success
+            break;
+        }
+
+        perror("connect");
+        ::close(recv_sock_fd);
+    }
+    freeaddrinfo(res);
+
+    return ret;
+}
+
+int TCPClient::Receive(char *buffer, size_t len) {
+    ssize_t received = ::recv(recv_sock_fd, buffer, len - 1, 0);
+
+    if (received < 0) {
+        switch (errno) {
+        case EAGAIN:
+            // No data available, not an error
+            return 0;
+        default:
+            perror("recv");
+            return -1;
+        }
+    }
+
+    buffer[received] = '\0'; // make it a C-string for printing
+
+    return received;
+}
+
+ssize_t TCPClient::Send(const char *buffer, size_t len) {
+    if (recv_sock_fd < 0) {
+        std::cerr << "Error: cannot send if no client is connected\n";
+    }
+
+    size_t total = 0;
+    while (total < len) {
+        ssize_t sent = ::send(recv_sock_fd, buffer + total, len - total, 0);
+        if (sent < 0 && errno != EINTR) {
+            perror("sendto");
+            return -1;
+        }
+
+        total += sent;
+    }
+
+    return total;
+}
